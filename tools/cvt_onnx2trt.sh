29261daa4d5a6f4d31362abc6fd69469ce45f27d
Teddywesside1 <18852056629@163.com>
2025-03-11 16:17:17 +0800

    Upload origin/optimized onnx model

--- tools/cvt_onnx2trt.sh
+++ tools/cvt_onnx2trt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Converting LightStereo onnx model to tensorrt ..."
-/usr/src/tensorrt/bin/trtexec --onnx=/workspace/models/lightstereo_s_sceneflow_general.onnx \
+/usr/src/tensorrt/bin/trtexec --onnx=/workspace/models/lightstereo_s_sceneflow_general_origin.onnx \
                               --saveEngine=/workspace/models/lightstereo_s_sceneflow_general.engine \
                               --fp16
